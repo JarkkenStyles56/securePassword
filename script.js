03b36adf6a3f63b39e10e2bbc40e263ddffa2dad
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-08-29 15:27:03 -0600

    Added prompt and confirms to JS file. Fixed Arrays.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let alpha = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-let num = ("0","1","2","3","4","5","6","7","8","9");
+const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-let specChar = ("!","@","#","$","%","^","&","*","(",")");
+const specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+const allChars = []
+
+let pwLength = prompt("How many characters do you want your password to be?")
+
+let wantUpper = confirm("Do you want Uppercase?")
+
+let wantSpecial = confirm("Do you want Special Characters?")
