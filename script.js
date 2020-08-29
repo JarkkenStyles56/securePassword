95c14762e1a4e96d947ea18ca585b6a2d70edc09
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-08-29 15:32:33 -0600

    Transfered actual homework files, added js from my own work, to the js file that I'm actually suppose to use.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Constants
+const generateBtn = document.querySelector("#generate");
 const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -11,3 +13,35 @@ let pwLength = prompt("How many characters do you want your password to be?")
 let wantUpper = confirm("Do you want Uppercase?")
 
 let wantSpecial = confirm("Do you want Special Characters?")
+
+// Functions
+
+/**
+ * writes a generated password to the #password
+ */
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+/**
+ * generate a password based on certain criteria
+ */
+function generatePassword(){
+
+  // Password is currently blank! We need to make a better one
+  let password = "";
+
+
+
+
+
+  return password;
+}
+
+// Main Process
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
