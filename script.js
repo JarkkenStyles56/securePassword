2a88e3c55f45ec15eba7031c17f56fb7043c5fff
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-08-29 20:46:42 -0600

    Split my character strings into arrays, concatenated my arrays, formatted script to make it easier to understand what is firing off and when.

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 // Constants
 const generateBtn = document.querySelector("#generate");
-const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let nums = "0123456789";
+let specChars = "!@#$%^&*()";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+let splitLower = lowerChars.split("");
+let splitUpper = upperChars.split("");
+let splitSpec = specChars.split("");
+let splitNums = nums.split("");
 
-const allChars = []
 
-let pwLength = prompt("How many characters do you want your password to be?")
+let allChars = [];
 
-let wantUpper = confirm("Do you want Uppercase?")
+let pwLength = prompt("How many characters do you want your password to be?");
 
-let wantSpecial = confirm("Do you want Special Characters?")
+let wantLower = confirm("do you need Lowercase?");
+
+let wantUpper = confirm("Do you need Uppercase?");
+
+let wantSpecial = confirm("Do you need Special Characters?");
+
+let wantNumbers = confirm("Do you need Numbers?");
+
+if (pwLength < 8 || pwLength > 128) {
+
+    alert("Password must be between 8 and 128 characters long")
+
+}
+
+if (wantLower) {
+    allChars = allChars.concat(splitLower);
+
+}
+
+if (wantUpper) {
+    allChars = allChars.concat(splitUpper);
+
+}
+
+if (wantSpecial) {
+    allChars = allChars.concat(splitSpec);
+
+}
+
+if (wantNumbers) {
+    allChars = allChars.concat(splitNums);
+
+}
+
+else {
+    alert("Password must contain numbers, lowercase, uppercase, and special characters!")
+
+}
+
+console.log(allChars)
 
 // Functions
 
@@ -20,28 +63,30 @@ let wantSpecial = confirm("Do you want Special Characters?")
  * writes a generated password to the #password
  */
 function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+    const password = generatePassword();
+    const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+    passwordText.value = password;
 }
 
 /**
  * generate a password based on certain criteria
  */
-function generatePassword(){
+function generatePassword() {
 
-  // Password is currently blank! We need to make a better one
-  let password = "";
-
-
+    // Password is currently blank! We need to make a better one
+    let password = "";
 
 
 
-  return password;
+
+
+    return password;
 }
 
 // Main Process
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
